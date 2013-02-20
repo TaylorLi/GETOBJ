5bf943a0eb27cce634003eaa8bcf5cd3d2d1fc15
EagleDu <flyeagledu@163.com>
2013-02-20 23:25:18 +0800

    add month picker

--- Pedometer/ProjectPrivate/UI/PEDBarchartViewController.m
+++ Pedometer/ProjectPrivate/UI/PEDBarchartViewController.m
@@ -10,6 +10,8 @@
 
 @implementation PEDBarchartViewController
 
+@synthesize graphicHostView;
+@synthesize monthSelectView;
 @synthesize timer;
 
 -(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
@@ -23,6 +25,8 @@
         UIImage *tabbarImage = [UIImage imageNamed:@"second.png"] ;
         UITabBarItem *barItem = [[UITabBarItem alloc]initWithTitle:@"" image:tabbarImage tag:4];
         self.tabBarItem = barItem;
+        monthArray = [NSMutableArray arrayWithObjects:@"All", @"Today", @"Thursday",
+                       @"Wednesday", @"Tuesday", @"Monday", nil];
     }
     return self;
 }
@@ -35,9 +39,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    UIImageView *bgImage = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 320, 410)];
-    bgImage.image = [UIImage imageNamed:@"barchart.bmp"] ;
-    [self.view addSubview:bgImage];
+	CGFloat pickerHeight = 40.0f;
+    CGFloat width=[UIScreen mainScreen].bounds.size.width;
+	CGFloat x = 0;
+	CGFloat y = 331.0f;
+	CGRect tmpFrame = CGRectMake(x, y, width, pickerHeight);
+    
+	monthSelectView = [[V8HorizontalPickerView alloc] initWithFrame:tmpFrame];
+    monthSelectView.backgroundColor   = [UIColor darkGrayColor];
+	monthSelectView.selectedTextColor = [UIColor whiteColor];
+	monthSelectView.textColor   = [UIColor grayColor];
+	monthSelectView.delegate    = self;
+	monthSelectView.dataSource  = self;
+	monthSelectView.elementFont = [UIFont boldSystemFontOfSize:11.0f];
+    monthSelectView.selectedElementFont=[UIFont boldSystemFontOfSize:14.0f];
+	monthSelectView.selectionPoint = CGPointMake(tmpFrame.size.width/2, 0);
+    [self.view addSubview:monthSelectView];
 }
 
 -(void)viewDidAppear:(BOOL)animated
@@ -47,6 +64,7 @@
     self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self
                                                 selector:@selector(timerFired) userInfo:nil repeats:YES];
 #endif
+    [monthSelectView scrollToElement:0 animated:NO];
 }
 
 - (void)drawRect:(CGRect)rect
@@ -68,7 +86,7 @@
     barChart = [[CPTXYGraph alloc] initWithFrame:CGRectZero];
     CPTTheme *theme = [CPTTheme themeNamed:kCPTPlainWhiteTheme];
     [barChart applyTheme:theme];
-    CPTGraphHostingView *hostingView = (CPTGraphHostingView *)self.view;
+    CPTGraphHostingView *hostingView = (CPTGraphHostingView *)self.graphicHostView;
     hostingView.hostedGraph = barChart;
     
     // Border
@@ -249,4 +267,34 @@
     return num;
 }
 
+#pragma mark - HorizontalPickerView DataSource Methods
+- (NSInteger)numberOfElementsInHorizontalPickerView:(V8HorizontalPickerView *)picker {
+	return [monthArray count];
+}
+
+#pragma mark - HorizontalPickerView Delegate Methods
+- (NSString *)horizontalPickerView:(V8HorizontalPickerView *)picker titleForElementAtIndex:(NSInteger)index {
+	return [monthArray objectAtIndex:index];
+}
+
+- (NSInteger) horizontalPickerView:(V8HorizontalPickerView *)picker widthForElementAtIndex:(NSInteger)index {
+	CGSize constrainedSize = CGSizeMake(MAXFLOAT, MAXFLOAT);
+	NSString *text = [monthArray objectAtIndex:index];
+    CGFloat fontSize = picker.currentSelectedIndex==index?14.0f:11.0f;
+	CGSize textSize = [text sizeWithFont:[UIFont boldSystemFontOfSize:fontSize]
+					   constrainedToSize:constrainedSize
+						   lineBreakMode:UILineBreakModeWordWrap];
+	return textSize.width + 20.0f; // 20px padding on each side
+}
+
+- (void)horizontalPickerView:(V8HorizontalPickerView *)picker didSelectElementAtIndex:(NSInteger)index {
+	
+}
+
+
+- (void)viewDidUnload {
+    [self setGraphicHostView:nil];
+    [self setMonthSelectView:nil];
+    [super viewDidUnload];
+}
 @end
